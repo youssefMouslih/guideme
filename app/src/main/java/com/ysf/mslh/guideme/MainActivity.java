5634c3ac150c3216b88502d4207eb8668006208d
github_username <email_address>
2025-03-25 11:45:24 +0000

    add bottom menu navigation

--- app/src/main/java/com/ysf/mslh/guideme/MainActivity.java
+++ app/src/main/java/com/ysf/mslh/guideme/MainActivity.java
@@ -1,14 +1,80 @@
 package com.ysf.mslh.guideme;
 
 import androidx.appcompat.app.AppCompatActivity;
-
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
 import android.os.Bundle;
+import com.ashokvarma.bottomnavigation.BottomNavigationBar;
+import com.ashokvarma.bottomnavigation.BottomNavigationItem;
+import com.ysf.mslh.guideme.fragments.CommunityFragment;
+import com.ysf.mslh.guideme.fragments.DiscoverFragment;
+import com.ysf.mslh.guideme.fragments.HomeFragment;
+import com.ysf.mslh.guideme.fragments.ProfileFragment;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements BottomNavigationBar.OnTabSelectedListener {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        BottomNavigationBar bottomNavigationBar = findViewById(R.id.bottom_navigation_bar);
+
+        bottomNavigationBar
+                .setMode(BottomNavigationBar.MODE_FIXED)
+                .setBackgroundStyle(BottomNavigationBar.BACKGROUND_STYLE_STATIC)
+                .setBarBackgroundColor(R.color.white);
+
+        bottomNavigationBar
+                .addItem(new BottomNavigationItem(R.drawable.ic_home, "Home")
+                        .setActiveColorResource(R.color.main_color))
+                .addItem(new BottomNavigationItem(R.drawable.ic_baseline_map_24, "Discover")
+                        .setActiveColorResource(R.color.main_color))
+                .addItem(new BottomNavigationItem(R.drawable.ic_baseline_people_24, "Community")
+                        .setActiveColorResource(R.color.main_color))
+                .addItem(new BottomNavigationItem(R.drawable.ic_baseline_person_24, "Profile")
+                        .setActiveColorResource(R.color.main_color))
+                .setFirstSelectedPosition(0)
+                .initialise();
+
+        bottomNavigationBar.setTabSelectedListener(this);
+
+        // Afficher le premier fragment (Home)
+        loadFragment(new HomeFragment());
     }
+
+    private void loadFragment(Fragment fragment) {
+        FragmentManager fragmentManager = getSupportFragmentManager();
+        fragmentManager.beginTransaction()
+                .replace(R.id.fragment_container, fragment)
+                .commit();
+    }
+
+    @Override
+    public void onTabSelected(int position) {
+        Fragment selectedFragment = null;
+        switch (position) {
+            case 0:
+                selectedFragment = new HomeFragment();
+                break;
+            case 1:
+                selectedFragment = new DiscoverFragment();
+                break;
+            case 2:
+                selectedFragment = new CommunityFragment();
+                break;
+            case 3:
+                selectedFragment = new ProfileFragment();
+                break;
+        }
+        if (selectedFragment != null) {
+            loadFragment(selectedFragment);
+        }
+    }
+
+    @Override
+    public void onTabUnselected(int position) {}
+
+    @Override
+    public void onTabReselected(int position) {}
 }
